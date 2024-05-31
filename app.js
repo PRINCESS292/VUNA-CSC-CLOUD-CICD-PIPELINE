8be426b349ecdc0d48f5729b4b66cd7a5e2dc046
PRINCESS292 <olofuprincess@gmail.com>
2024-05-31 14:44:25 +0100

    main

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const port = 8080;
 
 const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
